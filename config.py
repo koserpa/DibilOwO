cc0fd31a481d484a686e5b120c329872df6b1e94
koserpa <koserpa44@gmail.com>
2026-02-20 10:15:03 +0100

    0.2

--- config.py
+++ config.py
@@ -4,23 +4,17 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    # Discord
     TOKEN = os.getenv('DISCORD_TOKEN')
     
-    # Lavalink (для Railway використовуйте публічний сервер або власний)
-    # Можна використати безкоштовні публічні сервери або встановити свій
-    LAVALINK_HOST = os.getenv('LAVALINK_HOST', 'lavalink-v4.teramont.net')
+    # ОНОВЛЕНО: Новий робочий сервер
+    LAVALINK_HOST = os.getenv('LAVALINK_HOST', 'lavalinkv4.serenetia.com')
     LAVALINK_PORT = int(os.getenv('LAVALINK_PORT', '443'))
-    LAVALINK_PASSWORD = os.getenv('LAVALINK_PASSWORD', 'eHKuFcz67k4lBS64')
+    LAVALINK_PASSWORD = os.getenv('LAVALINK_PASSWORD', 'https://dsc.gg/ajidevserver')
     LAVALINK_SSL = os.getenv('LAVALINK_SSL', 'true').lower() == 'true'
     
-    # Spotify API (для пошуку та плейлистів)
     SPOTIFY_CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID')
     SPOTIFY_CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
     
-    # Налаштування бота
     DEFAULT_VOLUME = 50
     MAX_QUEUE_SIZE = 100
-    
-    # Проксі (опціонально, для обходу блокувань)
     YTDL_PROXY = os.getenv('YTDL_PROXY', '')
